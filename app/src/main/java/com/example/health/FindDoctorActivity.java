13e878087960277b737f1acbfab9d14e35745732
dipika <rohiladipika1995@gmail.com>
2026-03-29 13:53:30 +0530

    fp

--- app/src/main/java/com/example/health/FindDoctorActivity.java
+++ app/src/main/java/com/example/health/FindDoctorActivity.java
@@ -8,7 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.cardview.widget.CardView;
 
 public class FindDoctorActivity extends AppCompatActivity {
@@ -18,7 +22,14 @@ public class FindDoctorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_find_doctor);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         editTextDisease = findViewById(R.id.editTextFDDisease);
 
